b1175c66ba2927fc9f314e5df9c8d7980b3cdff8
MJbidram <mjbidram1378@gmail.com>
2022-12-16 23:55:17 +0330

    fix repositories cods

--- lib/repositories/repositories.dart
+++ lib/repositories/repositories.dart
@@ -7,10 +7,32 @@ import 'package:flutter/src/widgets/container.dart';
 import '../models/models.dart';
 
 class Repositories {
+  Map<String, String> requestHeaders = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json',
+  };
   Future<HadisModel> getHadisData() async {
-    var response = await Dio().get(ApiAddress.hadisAddress);
-    var jsonObject = response.data;
-    var hadis = HadisModel.fromeJsonMap(jsonObject);
+    late HadisModel hadis;
+    try {
+      var response = await Dio().get(ApiAddress.hadisAddress);
+
+      if (response.statusCode == 200) {
+        var jsonObject = response.data;
+        hadis = HadisModel.fromeJsonMap(jsonObject);
+      } else {
+        print('exception throwed in Try');
+      }
+    } on DioError catch (e) {
+      if (e.response != null) {
+        print('Dio error!');
+        print('STATUS: ${e.response?.statusCode}');
+        print('DATA: ${e.response?.data}');
+        print('HEADERS: ${e.response?.headers}');
+      } else {
+        print('Error sending request!');
+        print(e.message);
+      }
+    }
     return hadis;
   }
 
@@ -40,32 +62,84 @@ class Repositories {
 
   // Future<List<Home>>
   Future<List> getHomeData() async {
-    var response = await Dio().get(ApiAddress.homeAddress);
+    late List<HomePooyeshModel> homePooyeshList;
+    late List<HomeProjectsModel> homeProjectsList;
+    late HadisModel hadis;
+    try {
+      var response = await Dio().get(ApiAddress.homeAddress);
 
-    List<HomePooyeshModel> homePooyeshList = response.data['data']['pooyeshes']
-        .map((jsonObject) => HomePooyeshModel.fromJsonMap(jsonObject))
-        .toList()
-        .cast<HomePooyeshModel>();
+      if (response.statusCode == 200) {
+        homePooyeshList = response.data['data']['pooyeshes']
+            .map((jsonObject) => HomePooyeshModel.fromJsonMap(jsonObject))
+            .toList()
+            .cast<HomePooyeshModel>();
 
-    List<HomeProjectsModel> homeProjectsList = response.data['data']['projects']
-        .map((jsonObject) => HomeProjectsModel.fromJsonMap(jsonObject))
-        .toList()
-        .cast<HomeProjectsModel>();
+        homeProjectsList = response.data['data']['projects']
+            .map((jsonObject) => HomeProjectsModel.fromJsonMap(jsonObject))
+            .toList()
+            .cast<HomeProjectsModel>();
 
-    var jsonObject = response.data['data']['hadis'];
+        var jsonObject = response.data['data']['hadis'];
 
-    var hadis = HadisModel.fromeJsonMap(jsonObject);
+        hadis = HadisModel.fromeJsonMap(jsonObject);
+      } else {
+        print('exception throwed in Try');
+      }
+    } on DioError catch (e) {
+      if (e.response != null) {
+        print('Dio error!');
+        print('STATUS: ${e.response?.statusCode}');
+        print('DATA: ${e.response?.data}');
+        print('HEADERS: ${e.response?.headers}');
+      } else {
+        print('Error sending request!');
+        print(e.message);
+      }
+    }
+    await dioPostHeader(ApiAddress.homeAddress);
 
     return [homePooyeshList, homeProjectsList, hadis];
   }
 
   Future<List<NewsModel>> gethomePageNews() async {
-    var response = await Dio().get(ApiAddress.newsAddressHome);
-    List<NewsModel> homeNews = response.data
-        .map((jsonObject) => NewsModel.fromJsonObject(jsonObject))
-        .toList()
-        .cast<NewsModel>();
+    late List<NewsModel> homeNews;
+
+    try {
+      var response = await Dio().get(ApiAddress.newsAddressHome);
+
+      if (response.statusCode == 200) {
+        homeNews = response.data
+            .map((jsonObject) => NewsModel.fromJsonObject(jsonObject))
+            .toList()
+            .cast<NewsModel>();
+      } else {
+        print('exception throwed in Try');
+      }
+    } on DioError catch (e) {
+      if (e.response != null) {
+        print('Dio error!');
+        print('STATUS: ${e.response?.statusCode}');
+        print('DATA: ${e.response?.data}');
+        print('HEADERS: ${e.response?.headers}');
+      } else {
+        print('Error sending request!');
+        print(e.message);
+      }
+    }
+    await dioPostHeader(ApiAddress.newsAddressHome);
+
     return homeNews;
+  }
+
+  Future<void> dioPostHeader(String address) async {
+    var post = await Dio().post(address,
+        options: Options(
+          headers: requestHeaders,
+          validateStatus: (_) => true,
+          contentType: Headers.jsonContentType,
+          responseType: ResponseType.json,
+        ));
+    print(post);
   }
 }
 
